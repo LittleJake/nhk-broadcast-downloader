0d5b5f4b9a49f01261bfa948dfbd931e9eed12b0
Jake Liu <13583702+LittleJake@users.noreply.github.com>
2021-11-19 17:14:20 +0800

    Update NHK.py
    1. 增加年级别的目录
    2. 增加历史索引的功能（从今天开始的） #1

--- NHK.py
+++ NHK.py
@@ -6,56 +6,79 @@ from xml.dom.minidom import parseString
 import requests
 from utils.setting import *
 import shutil
+from datetime import datetime
 
 # under Python3
 if sys.version_info.major < 3:
     reload(sys)
     sys.setdefaultencoding('utf8')
 
-dom = parseString(requests.get(RSS_URL).text)
-rss = parseString(requests.get(RSS_URL).text)
-for node in rss.getElementsByTagName('item'):
-    rss.getElementsByTagName('channel')[0].removeChild(node)
 
-for node in dom.getElementsByTagName('item'):
-    name = node.getElementsByTagName('title')[0].childNodes[0].data
-    url = node.getElementsByTagName('enclosure')[0].getAttribute('url')
+def handle_nhk():
+    guid = set()
+    dom = parseString(requests.get(RSS_URL).text)
+
+    # load previous xml
     try:
-        date, filename = name.split(" ")
-        try:
-            os.mkdir(DOWNLOAD_DIR + date)
-        except:
-            pass
+        with codecs.open("rss.xml", "r", "utf-8") as fp:
+            rss = parseString(fp.read())
+    except:
+        rss = parseString(requests.get(RSS_URL).text)
 
-        try:
-            response = requests.get(url)
-            with open(DOWNLOAD_DIR + "%s/%s.mp3" % (date, filename), "wb") as f:
-                f.write(response.content)
-                print("%s 下载成功" % filename)
-        except Exception:
-            print("%s 下载失败" % filename)
+    # remove duplicate item
+    for item in rss.getElementsByTagName('item'):
+        guid.add(item.getElementsByTagName('guid')[0].childNodes[0].data)
 
-        node.getElementsByTagName('enclosure')[0].setAttribute('url', BASE_URL + "%s/%s.mp3" % (date, filename))
-        rss.getElementsByTagName('channel')[0].appendChild(node)
+    # proceed
+    for item in dom.getElementsByTagName('item'):
+        if item.getElementsByTagName('guid')[0].childNodes[0].data in guid:
+            continue
+
+        date = item.getElementsByTagName('pubDate')[0].childNodes[0].data
+        filename = item.getElementsByTagName('title')[0].childNodes[0].data
+        url = item.getElementsByTagName('enclosure')[0].getAttribute('url')
+        try:
+            date = datetime.strptime(date, '%a, %d %b %Y %H:%M:%S +0900')
+            try:
+                os.makedirs("{}{}".format(DOWNLOAD_DIR, date.strftime('%Y年/%m月%d日/')))
+            except:
+                pass
+
+            try:
+                response = requests.get(url)
+                with open("{}{}{}.mp3".format(DOWNLOAD_DIR, date.strftime('%Y年/%m月%d日/'), filename), "wb") as f:
+                    f.write(response.content)
+                    print("%s 下载成功" % filename)
+            except Exception:
+                print("%s 下载失败" % filename)
+
+            item.getElementsByTagName('enclosure')[0].\
+                setAttribute('url', BASE_URL + "{}{}.mp3".format(date.strftime('%Y年/%m月%d日/'), filename))
+            rss.getElementsByTagName('channel')[0].appendChild(item)
+        except Exception as e:
+            print(e)
+            # pass
+    # save
+    with codecs.open("rss.xml", "w", "utf-8") as fp:
+        fp.write('\n'.join([line for line in rss.toprettyxml(indent=' '*2).split('\n') if line.strip()]))
+
+
+def additional_func():
+    try:
+        if MOVE_DIR:
+            shutil.move("%s/*" % DOWNLOAD_DIR, MOVE_DIR)
+            print("移动成功")
+        elif MOVE_COMMAND:
+            os.system(MOVE_COMMAND.format(DOWNLOAD_DIR))
+            print("移动命令执行成功")
+
+        # rss
+        if COPY_COMMAND:
+            os.system(COPY_COMMAND.format("rss.xml", "rss.xml"))
+            print("复制命令执行成功")
     except Exception as e:
         print(e)
-        # pass
 
 
-with codecs.open("rss.xml", "w", "utf-8") as fp:
-    rss.writexml(fp)
-
-try:
-    if MOVE_DIR:
-        shutil.move("%s/*" % DOWNLOAD_DIR, MOVE_DIR)
-        print("移动成功")
-    elif MOVE_COMMAND:
-        os.system(MOVE_COMMAND.format(DOWNLOAD_DIR))
-        print("移动命令执行成功")
-
-    # rss
-    if COPY_COMMAND:
-        os.system(COPY_COMMAND.format("rss.xml", "rss.xml"))
-        print("复制命令执行成功")
-except Exception as e:
-    print(e)
+handle_nhk()
+additional_func()
